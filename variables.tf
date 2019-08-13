77b68f4dfa66344a34848c839daacdad534ee61c
Damon Conway <vstraylight@gmail.com>
2019-08-13 13:39:48 -0700

    Results of running terraform 0.12upgrade
    * Used flatten to make one dimensional arrays where necessary
    * Updated .travis.yml to install terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "vpc_id" {}
+
 variable "ssh_key_name" {}
 
 variable "name" {
@@ -22,16 +23,16 @@ variable "max_size" {
 
 variable "subnet_ids" {
   description = "Subnets for RabbitMQ nodes"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "nodes_additional_security_group_ids" {
-  type    = "list"
+  type    = list(string)
   default = []
 }
 
 variable "elb_additional_security_group_ids" {
-  type    = "list"
+  type    = list(string)
   default = []
 }
 
@@ -50,3 +51,4 @@ variable "instance_volume_size" {
 variable "instance_volume_iops" {
   default = "0"
 }
+
